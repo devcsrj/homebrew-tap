1c39cf245986f0fb7bac4b33506a233a7ce057c0
devcsrj <devcsrj@apache.org>
2019-09-13 12:52:45 +0800

    Brew formula update for jarl version 1.0.0

--- jarl.rb
+++ jarl.rb
@@ -7,11 +7,11 @@ class Jarl < Formula
 
   if OS.mac?
     url "https://github.com/devcsrj/jarl/releases/download/1.0.0/jarl_1.0.0_Darwin_x86_64.tar.gz"
-    sha256 "aed37507eb451e928a0a5bd69ed0307481eaaa7a320e829fa4c54ea3206420a9"
+    sha256 "53fdfaf1ef5a08e2e6bf370369ec679eaea1e69768a46847bc5c6d7e43908ac4"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/devcsrj/jarl/releases/download/1.0.0/jarl_1.0.0_Linux_x86_64.tar.gz"
-      sha256 "a38d788c057978aff508edbdfa926960157832db881f2bdbd8e728fca91f0614"
+      sha256 "5ff029e15892216360d76dad2cb0861ef1f0a4ae724d7b698294623fb02fdd30"
     end
   end
 
